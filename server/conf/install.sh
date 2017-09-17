d025e77aa22479cc073b39f7f2447fccc476e2bf
wayfarerx <x@wayfarerx.net>
2017-09-17 10:23:08 -0600

    Create EC2 install-on-first-boot script.

---
+++ server/conf/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# This will install the circumvolve server on an EC2 instance when it first launches.
+
+# Upgrade to Java 1.8
+yum -y install java-1.8.0
+yum -y remove java-1.7.0-openjdk
+
+# Create the circumvolve user.
+useradd -d /opt/circumvolve -m circumvolve
+echo "su -c '/opt/circumvolve/circumvolve' circumvolve &" >> /etc/rc.local
+
+# Configure the boot script.
+su circumvolve
+cd
+cat > circumvolve <<- EOM
+#!/bin/bash
+cd
+aws s3 cp s3://circumvolve.wayfarerx.net/token.txt .
+aws s3 cp s3://circumvolve.wayfarerx.net/circumvolve.jar .
+cat token.txt | java -jar circumvolve.jar circumvolve.wayfarerx.net guilds
+EOM
+chmod +x circumvolve
